1cef590f3e2a88359cfecb5dcdd53f8a76fb79c8
Rahul <rahulsinghkgg@gmail.com>
2019-12-28 14:20:22 +0530

    basic setup done

---
+++ resources/views/welcome/welcome.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.welcome')
+@section('title','Welcome')
+@section('meta')
+	
+@endsection
+@section('script1')
+
+@endsection
+@section('script2')
+<script src="{{asset('unicat/plugins/greensock/TweenMax.min.js')}}"></script>
+<script src="{{asset('unicat/plugins/greensock/TimelineMax.min.js')}}"></script>
+<script src="{{asset('unicat/plugins/scrollmagic/ScrollMagic.min.js')}}"></script>
+<script src="{{asset('unicat/plugins/greensock/animation.gsap.min.js')}}"></script>
+<script src="{{asset('unicat/plugins/greensock/ScrollToPlugin.min.js')}}"></script>
+<script src="{{asset('unicat/plugins/OwlCarousel2-2.2.1/owl.carousel.js')}}"></script>
+<script src="{{asset('unicat/plugins/easing/easing.js')}}"></script>
+<script src="{{asset('unicat/plugins/parallax-js-master/parallax.min.js')}}"></script>
+<script src="{{asset('unicat/js/custom.js')}}"></script>
+@endsection
+@section('style1')
+	
+@endsection
+@section('style2')
+<link rel="stylesheet" type="text/css" href="{{asset('unicat/plugins/OwlCarousel2-2.2.1/owl.carousel.css')}}">
+<link rel="stylesheet" type="text/css" href="{{asset('unicat/plugins/OwlCarousel2-2.2.1/owl.theme.default.css')}}">
+<link rel="stylesheet" type="text/css" href="{{asset('unicat/plugins/OwlCarousel2-2.2.1/animate.css')}}">
+<link rel="stylesheet" type="text/css" href="{{asset('unicat/styles/main_styles.css')}}">
+<link rel="stylesheet" type="text/css" href="{{asset('unicat/styles/responsive.css')}}">
+@endsection
+@section('body')
+	
+@endsection
